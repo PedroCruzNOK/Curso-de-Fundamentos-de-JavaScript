798dc8bd039a614ef13dbfef89c063387951cf8a
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2025-02-11 11:21:41 -0600

    ✨ Declaracion de variables

---
+++ clase-variables.js
@@ -0,0 +1,17 @@
+// La declaracion de variables debe de ser descriptiva  e intuitiva
+
+let cajaDeAndy = 'Woody';
+console.log(cajaDeAndy);
+
+// lo NO permitido en la declaracion de variables
+
+let c = 'Woody';
+let cda = 'Woody';
+let pcAndy = 'Woody';
+
+//lo permitido
+
+let primerJugueteDeAndy = 'Woody';
+let urlDeUsuario = 'https://www.google.com';
+let idDeUsuario = '1234567890';
+
