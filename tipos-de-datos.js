d3e2c6b66e4e8649cf0ccf765c19db6c390e99dd
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2025-02-11 11:31:51 -0600

    ✨ Tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,39 @@
+//PRIMITIVOS
+
+//string
+let nombre = 'Pedro';
+
+//number
+let edad = 25;
+
+//boolean
+let esMayorDeEdad = true;
+
+// null 
+let noHayValor = null;
+
+//undefined
+let noDefinido = undefined;
+
+//symbol
+let simbolo = Symbol('unico');
+
+//bigint
+let numeroGrande = 2n
+
+//COMPLEJOS
+
+//object
+let carro = {
+  color: 'Azul',
+  marca: 'Tesla',
+  modelo: 'Model s'
+}
+
+//array
+let frutas = ['manzana', 'uvas', 'mangos'];
+
+//function
+function saludar(valoresNecesarioDentroDeLaFuncion) {
+  //cuerpo
+}
